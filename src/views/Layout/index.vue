e6ee77ef1f368b8d579d2a7d2540122f5f399f97
申07 <13019707+xoxo000@user.noreply.gitee.com>
2024-07-28 20:14:55 +0800

    init

---
+++ src/views/Layout/index.vue
@@ -0,0 +1,4 @@
+<template>
+    我是首页
+    <router-view></router-view>
+</template>
---
+++ vite.config.js
@@ -0,0 +1,35 @@
+import { fileURLToPath, URL } from 'node:url'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    // 配置插件
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver({importStyle: 'sass'})],
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 自动导入定制化样式文件进行样式覆盖
+        additionalData: `
+          @use "@/styles/element/index.scss" as *;
+        `,
+      }
+    }
+  }
+})
